9c1d1d42cbb6b423d96b7754786419bac2b06f85
Mike Caplan <michael.caplan@henryscheinone.com>
2025-02-22 20:08:33 -0400

    bar

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,6 +13,54 @@ export default {
         }
     },
 
+    computed: {
+        goalPercent() {
+
+            let percent = 0;
+
+            for (let i = 0; i < this.donors.length; i++) {
+                percent += this.donors[i].total / 100;
+            }
+
+            if (percent > 0) {
+
+                if (percent > 5000) {
+                    return Math.floor((percent / 10000) * 100);
+                } else {
+                    return Math.floor((percent / 5000) * 100);
+                }
+
+            }
+
+            return 0;
+        },
+
+        total() {
+            let total = 0;
+
+            for (let i = 0; i < this.donors.length; i++) {
+                total += this.donors[i].total / 100;
+            }
+
+            return '$' + total;
+        },
+
+        goal() {
+            let goal = 5000;
+            let total = 0;
+
+            for (let i = 0; i < this.donors.length; i++) {
+                total += this.donors[i].total / 100;
+            }
+
+            if (total >= 5000) {
+                goal = 10000;
+            }
+
+            return '$' + goal;
+        }
+    },
+
     async created() {
 
         appwrite.subscribe(
@@ -153,9 +201,10 @@ export default {
         
             <div class="d-flex ga-5 flex-1-1-auto flex-wrap justify-center px-4">
                 <stripe-buy-button
-                    buy-button-id="buy_btn_1Qrj9NFSy4s51StpTW5GBs5j"
+                    buy-button-id="buy_btn_1QvRz4FSy4s51StpxnXxsOhQ"
                     publishable-key="pk_test_51QrIh0FSy4s51StpeTydo1LoHGB19jHUUGZuiYOjHsp15TAKoWony99B2H8MODUJcpkT0DgRMMPn34GnG6J3IAlc00vIkX2am6"
-                ></stripe-buy-button>
+                >
+                </stripe-buy-button>
             </div>
         </v-sheet>
           
@@ -531,15 +580,15 @@ export default {
       
     <v-bottom-navigation density="compact">
         <v-progress-linear
-            model-value="10"
+            :model-value="goalPercent"
             color="primary"
             height="25"
             class="ma-2"
             striped
         >
-            <strong>$500 / $5000</strong>
+            <strong>{{ total }} / {{ goal }}</strong>
         </v-progress-linear>
-  </v-bottom-navigation>
+    </v-bottom-navigation>
   
     <v-fab
         size="large"
@@ -622,7 +671,7 @@ export default {
                 target="_blank"
                 icon 
                 key="8"
-                href="https://threads.net/intent/post?text=Hey%20friends.%20Super%20great%20project%20offering%20free%20skateboarding%20programming%20to%20youth%20facing%20adversities%20in%20our%20community%20looking%20for%20some%20help%20to%20get%20off%20the%20ground.%20Can%20you%20pitch%20in%3F%20%0A%0Ahttps%3A%2F%2F50.michaelcaplan.com"
+                href="https://threads.net/intent/post?text=Hey+friends.+Super+great+project+offering+free+skateboarding+programming+to+youth+facing+adversities+in+our+community+looking+for+some+help+to+get+off+the+ground.+Can+you+pitch+in%3F+%0A%0Ahttps%3A%2F%2F50.michaelcaplan.com"
             >
                 <v-icon>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path fill="#ffffff" d="M331.5 235.7c2.2 .9 4.2 1.9 6.3 2.8c29.2 14.1 50.6 35.2 61.8 61.4c15.7 36.5 17.2 95.8-30.3 143.2c-36.2 36.2-80.3 52.5-142.6 53h-.3c-70.2-.5-124.1-24.1-160.4-70.2c-32.3-41-48.9-98.1-49.5-169.6V256v-.2C17 184.3 33.6 127.2 65.9 86.2C102.2 40.1 156.2 16.5 226.4 16h.3c70.3 .5 124.9 24 162.3 69.9c18.4 22.7 32 50 40.6 81.7l-40.4 10.8c-7.1-25.8-17.8-47.8-32.2-65.4c-29.2-35.8-73-54.2-130.5-54.6c-57 .5-100.1 18.8-128.2 54.4C72.1 146.1 58.5 194.3 58 256c.5 61.7 14.1 109.9 40.3 143.3c28 35.6 71.2 53.9 128.2 54.4c51.4-.4 85.4-12.6 113.7-40.9c32.3-32.2 31.7-71.8 21.4-95.9c-6.1-14.2-17.1-26-31.9-34.9c-3.7 26.9-11.8 48.3-24.7 64.8c-17.1 21.8-41.4 33.6-72.7 35.3c-23.6 1.3-46.3-4.4-63.9-16c-20.8-13.8-33-34.8-34.3-59.3c-2.5-48.3 35.7-83 95.2-86.4c21.1-1.2 40.9-.3 59.2 2.8c-2.4-14.8-7.3-26.6-14.6-35.2c-10-11.7-25.6-17.7-46.2-17.8H227c-16.6 0-39 4.6-53.3 26.3l-34.4-23.6c19.2-29.1 50.3-45.1 87.8-45.1h.8c62.6 .4 99.9 39.5 103.7 107.7l-.2 .2zm-156 68.8c1.3 25.1 28.4 36.8 54.6 35.3c25.6-1.4 54.6-11.4 59.5-73.2c-13.2-2.9-27.8-4.4-43.4-4.4c-4.8 0-9.6 .1-14.4 .4c-42.9 2.4-57.2 23.2-56.2 41.8l-.1 .1z"/></svg>    
