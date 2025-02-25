b142306caceffcff6f21470bc88c1721d9558a66
Mike Caplan <michael.caplan@henryscheinone.com>
2025-02-25 16:28:06 -0400

    mike

--- assets/js/app.js
+++ assets/js/app.js
@@ -971,7 +971,7 @@ export default {
                                                     frameborder="0"
                                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                                     referrerpolicy="strict-origin-when-cross-origin" 
-                                                    allowfullscreen
+                                                    allowfullscreen 
                                                 ></iframe>
                                             </div>
                                         
